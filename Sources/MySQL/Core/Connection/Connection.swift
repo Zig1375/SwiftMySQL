fc08d2be4b5fd2371d3ad3fec275e248bcaded09
Zig1375 <zig@zig.zag>
2016-06-02 18:27:49 +0300

    Fix for swift 2016-05-03

--- Sources/MySQL/Core/Connection/Connection.swift
+++ Sources/MySQL/Core/Connection/Connection.swift
@@ -25,8 +25,9 @@ public class Connection {
             throw MysqlError.Error(error : getText(buf : mysql_error(self.connection)), errno : mysql_errno(self.connection));
         }
 
-        mysql_options(self.connection, MYSQL_SET_CHARSET_NAME, "utf8");
-        mysql_options(self.connection, MYSQL_INIT_COMMAND, "SET NAMES utf8");
+        print(mysql_options(self.connection, MYSQL_SET_CHARSET_NAME, "utf8"));
+        print(mysql_options(self.connection, MYSQL_INIT_COMMAND, "SET NAMES utf8"));
+        print(mysql_set_character_set(self.connection, "utf8"));
 
         self.state = ConnectionState.CONNECTED;
     }
